f2b51799e14fc5cb7e480cf588506fa61e16277f
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-11-09 00:52:58 +0200

    refactor: lualine theme

--- lua/lualine/themes/eyes.lua
+++ lua/lualine/themes/eyes.lua
@@ -1,35 +1,27 @@
-local hl = vim.api.nvim_get_hl(0, { name = "StatusLine" })
-local hlnc = vim.api.nvim_get_hl(0, { name = "StatusLineNC" })
+local utility = require("eyes.utility")
+
+local statusline_hl = utility.get_hl("StatusLine")
+local bg = string.format("#%06X", statusline_hl.bg)
+local fg = string.format("#%06X", statusline_hl.fg)
+local statusline_nc_hl = utility.get_hl("StatusLineNC")
+local nc_bg = string.format("#%06X", statusline_nc_hl.bg)
+local nc_fg = string.format("#%06X", statusline_nc_hl.fg)
+local sections = {
+	a = { bg = bg, fg = fg, gui = "bold" },
+	b = { bg = bg, fg = fg },
+	c = { bg = bg, fg = fg },
+}
+local sections_inactive = {
+	a = { bg = nc_bg, fg = nc_fg, gui = "bold" },
+	b = { bg = nc_bg, fg = nc_fg },
+	c = { bg = nc_bg, fg = nc_fg },
+}
 
 return {
-	normal = {
-		a = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-		c = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-	},
-	insert = {
-		a = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-		c = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-	},
-	visual = {
-		a = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-		c = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-	},
-	replace = {
-		a = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-		c = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-	},
-	command = {
-		a = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-		c = { bg = string.format("#%06X", hl.bg), fg = string.format("#%06X", hl.fg) },
-	},
-	inactive = {
-		a = { bg = string.format("#%06X", hlnc.bg), fg = string.format("#%06X", hlnc.fg), gui = "bold" },
-		b = { bg = string.format("#%06X", hlnc.bg), fg = string.format("#%06X", hlnc.fg) },
-		c = { bg = string.format("#%06X", hlnc.bg), fg = string.format("#%06X", hlnc.fg) },
-	},
+	normal = sections,
+	insert = sections,
+	visual = sections,
+	replace = sections,
+	command = sections,
+	inactive = sections_inactive,
 }
