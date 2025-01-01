a9f60938d6e95421f5e14b6b8d3091d44b68056d
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-01 07:16:13 +0200

    style(blink): add new highlights

--- lua/eyes/highlights/plugins/blink_cmp.lua
+++ lua/eyes/highlights/plugins/blink_cmp.lua
@@ -8,16 +8,23 @@ M.setup = function()
 	hl("BlinkCmpMenu", { link = "Pmenu" })
 	hl("BlinkCmpMenuBorder", { link = "FloatBorder" })
 	hl("BlinkCmpMenuSelection", { link = "PmenuSel" })
+
+	hl("BlinkCmpScrollBarThumb", { link = "PmenuThumb" })
+	hl("BlinkCmpScrollBarGutter", { link = "PmenuSbar" })
+
 	hl("BlinkCmpLabel", { link = "Pmenu" })
 	hl("BlinkCmpLabelDescription", { link = "Pmenu" })
 	hl("BlinkCmpLabelDetail", { link = "Pmenu" })
 	hl("BlinkCmpLabelDeprecated", extend("Comment", { strikethrough = true }))
 	hl("BlinkCmpLabelMatch", { link = "PmenuMatch" })
+
 	hl("BlinkCmpGhostText", { link = "NonText" })
 	hl("BlinkCmpKind", { link = "Icon" })
+
 	hl("BlinkCmpDoc", { link = "NormalFloat" })
 	hl("BlinkCmpDocBorder", { link = "FloatBorder" })
 	hl("BlinkCmpDocCursorLine", { link = "CursorLine" })
+
 	hl("BlinkCmpSignatureHelp", { link = "NormalFloat" })
 	hl("BlinkCmpSignatureHelpBorder", { link = "FloatBorder" })
 	hl("BlinkCmpSignatureHelpActiveParameter", { link = "LspSignatureActiveParameter" })
