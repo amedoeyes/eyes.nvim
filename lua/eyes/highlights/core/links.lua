3df788448dff96a48210327a12b8cc6fd99595a0
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-17 17:50:24 +0200

    style(links): remove background from buttons

--- lua/eyes/highlights/core/links.lua
+++ lua/eyes/highlights/core/links.lua
@@ -5,8 +5,8 @@ local palette = require("eyes.palette").palette
 
 M.setup = function()
 	hl("Border", { fg = palette.hex04 })
-	hl("Button", { fg = palette.hex06, bg = "bg" })
-	hl("ButtonActive", { fg = "fg", bg = "bg", bold = true })
+	hl("Button", { fg = palette.hex06 })
+	hl("ButtonActive", { fg = "fg", bold = true })
 	hl("Dim", { fg = palette.hex04 })
 	hl("Mute", { fg = palette.hex06 })
 	hl("Highlight", { fg = "fg", bg = palette.hex02 })
