5c156e7a278085338d4ce1964495f0c8afba238a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-01-26 12:04:32 +0200

    fix(lualine): transparency #4

--- lua/lualine/themes/eyes.lua
+++ lua/lualine/themes/eyes.lua
@@ -1,16 +1,20 @@
+local opts = require("eyes.config").options
 local utility = require("eyes.utility")
 
 local statusline_hl = utility.get_hl("StatusLine")
-local bg = string.format("#%06X", statusline_hl.bg)
+local bg = not opts.transparent and string.format("#%06X", statusline_hl.bg) or nil
 local fg = string.format("#%06X", statusline_hl.fg)
+
 local statusline_nc_hl = utility.get_hl("StatusLineNC")
-local nc_bg = string.format("#%06X", statusline_nc_hl.bg)
+local nc_bg = not opts.transparent and string.format("#%06X", statusline_nc_hl.bg) or nil
 local nc_fg = string.format("#%06X", statusline_nc_hl.fg)
+
 local sections = {
 	a = { bg = bg, fg = fg, gui = "bold" },
 	b = { bg = bg, fg = fg },
 	c = { bg = bg, fg = fg },
 }
+
 local sections_inactive = {
 	a = { bg = nc_bg, fg = nc_fg, gui = "bold" },
 	b = { bg = nc_bg, fg = nc_fg },
