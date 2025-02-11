0fcb22f48bf408fcb1bb5e5784ea8445547488cf
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-12 01:07:33 +0200

    feat(blink.cmp): update highlights

--- lua/eyes/highlights/plugins/blink_cmp.lua
+++ lua/eyes/highlights/plugins/blink_cmp.lua
@@ -1,33 +1,60 @@
 local M = {}
 
-local utility = require("eyes.utility")
-local hl = utility.hl
-local extend = utility.extend
+local hl = require("eyes.utility").hl
 
 M.setup = function()
 	hl("BlinkCmpMenu", "Pmenu")
 	hl("BlinkCmpMenuBorder", "FloatBorder")
 	hl("BlinkCmpMenuSelection", "PmenuSel")
 
-	hl("BlinkCmpScrollBarThumb", "PmenuThumb")
 	hl("BlinkCmpScrollBarGutter", "PmenuSbar")
+	hl("BlinkCmpScrollBarThumb", "PmenuThumb")
 
 	hl("BlinkCmpLabel", "Pmenu")
+	hl("BlinkCmpLabelDeprecated", "Deprecated")
 	hl("BlinkCmpLabelDescription", "Pmenu")
-	hl("BlinkCmpLabelDetail", "Pmenu")
-	hl("BlinkCmpLabelDeprecated", extend("Comment", { strikethrough = true }))
+	hl("BlinkCmpLabelDetail", "Mute")
 	hl("BlinkCmpLabelMatch", "PmenuMatch")
 
-	hl("BlinkCmpGhostText", "Dim")
 	hl("BlinkCmpKind", "Icon")
+	hl("BlinkCmpKindClass", "BlinkCmpKind")
+	hl("BlinkCmpKindColor", "BlinkCmpKind")
+	hl("BlinkCmpKindConstant", "BlinkCmpKind")
+	hl("BlinkCmpKindConstructor", "BlinkCmpKind")
+	hl("BlinkCmpKindEnum", "BlinkCmpKind")
+	hl("BlinkCmpKindEnumMember", "BlinkCmpKind")
+	hl("BlinkCmpKindEvent", "BlinkCmpKind")
+	hl("BlinkCmpKindField", "BlinkCmpKind")
+	hl("BlinkCmpKindFile", "BlinkCmpKind")
+	hl("BlinkCmpKindFolder", "BlinkCmpKind")
+	hl("BlinkCmpKindFunction", "BlinkCmpKind")
+	hl("BlinkCmpKindInterface", "BlinkCmpKind")
+	hl("BlinkCmpKindKeyword", "BlinkCmpKind")
+	hl("BlinkCmpKindMethod", "BlinkCmpKind")
+	hl("BlinkCmpKindModule", "BlinkCmpKind")
+	hl("BlinkCmpKindOperator", "BlinkCmpKind")
+	hl("BlinkCmpKindProperty", "BlinkCmpKind")
+	hl("BlinkCmpKindReference", "BlinkCmpKind")
+	hl("BlinkCmpKindSnippet", "BlinkCmpKind")
+	hl("BlinkCmpKindStruct", "BlinkCmpKind")
+	hl("BlinkCmpKindText", "BlinkCmpKind")
+	hl("BlinkCmpKindTypeParameter", "BlinkCmpKind")
+	hl("BlinkCmpKindUnit", "BlinkCmpKind")
+	hl("BlinkCmpKindValue", "BlinkCmpKind")
+	hl("BlinkCmpKindVariable", "BlinkCmpKind")
 
 	hl("BlinkCmpDoc", "NormalFloat")
 	hl("BlinkCmpDocBorder", "FloatBorder")
 	hl("BlinkCmpDocCursorLine", "CursorLine")
+	hl("BlinkCmpDocSeparator", "FloatBorder")
 
 	hl("BlinkCmpSignatureHelp", "NormalFloat")
-	hl("BlinkCmpSignatureHelpBorder", "FloatBorder")
 	hl("BlinkCmpSignatureHelpActiveParameter", "LspSignatureActiveParameter")
+	hl("BlinkCmpSignatureHelpBorder", "FloatBorder")
+
+	hl("BlinkCmpSource", "Dim")
+
+	hl("BlinkCmpGhostText", "Dim")
 end
 
 return M
