6f0642674ba22abdb7e0012dda9addf67dff1d44
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-11 20:36:00 +0200

    refactor: clean init

--- lua/eyes/init.lua
+++ lua/eyes/init.lua
@@ -1,19 +1,5 @@
 local M = {}
 
----@param opts? eyes.Options
-M.setup = function(opts)
-	local config = require("eyes.config")
-	config.setup(opts)
-end
-
----@param palette eyes.Palette
-M.load = function(palette)
-	local opts = require("eyes.config").options
-	M.palette = palette
-	if opts.extend.palette then
-		M.palette = vim.tbl_extend("force", M.palette, opts.extend.palette)
-	end
-	require("eyes.highlights").setup()
-end
+M.setup = require("eyes.config").setup
 
 return M
