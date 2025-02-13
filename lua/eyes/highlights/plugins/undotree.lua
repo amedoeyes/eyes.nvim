0971e68218a087251ba4bd84f427e1ea0b650c5d
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-02-13 14:00:45 +0200

    feat(undotree): update highlights

--- lua/eyes/highlights/plugins/undotree.lua
+++ lua/eyes/highlights/plugins/undotree.lua
@@ -1,10 +1,22 @@
 local M = {}
 
 local hl = require("eyes.utility").hl
-local palette = require("eyes.palette")
 
 M.setup = function()
-	hl("UndotreeNode", { fg = palette.hex10 })
+	hl("UndotreeBranch", "Mute")
+	hl("UndotreeCurrent", { fg = "fg" })
+	hl("UndotreeFirstNode", "Mute")
+	hl("UndotreeHead", "Mute")
+	hl("UndotreeHelp", "Dim")
+	hl("UndotreeHelpKey", { fg = "fg" })
+	hl("UndotreeHelpTitle", "Title")
+	hl("UndotreeNext", "Mute")
+	hl("UndotreeNode", "Mute")
+	hl("UndotreeNodeCurrent", { fg = "fg" })
+	hl("UndotreeSavedBig", "Bold")
+	hl("UndotreeSavedSmall", { fg = "fg" })
+	hl("UndotreeSeq", "Dim")
+	hl("UndotreeTimeStamp", "Mute")
 end
 
 return M
