d541232e9842ff30e0cf8e53cb086b35de76b10f
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-03-18 06:04:16 +0200

    revert: terminal shades

--- lua/eyes/highlights/core/terminal.lua
+++ lua/eyes/highlights/core/terminal.lua
@@ -2,19 +2,19 @@ local palette = require("eyes.palette")
 
 return function()
 	vim.g.terminal_color_0 = palette.hex00
-	vim.g.terminal_color_1 = palette.hex10
-	vim.g.terminal_color_2 = palette.hex10
-	vim.g.terminal_color_3 = palette.hex10
-	vim.g.terminal_color_4 = palette.hex10
-	vim.g.terminal_color_5 = palette.hex10
-	vim.g.terminal_color_6 = palette.hex10
+	vim.g.terminal_color_1 = palette.hex04
+	vim.g.terminal_color_2 = palette.hex06
+	vim.g.terminal_color_3 = palette.hex08
+	vim.g.terminal_color_4 = palette.hex04
+	vim.g.terminal_color_5 = palette.hex06
+	vim.g.terminal_color_6 = palette.hex08
 	vim.g.terminal_color_7 = palette.hex10
 	vim.g.terminal_color_8 = palette.hex00
-	vim.g.terminal_color_9 = palette.hex10
-	vim.g.terminal_color_10 = palette.hex10
-	vim.g.terminal_color_11 = palette.hex10
-	vim.g.terminal_color_12 = palette.hex10
-	vim.g.terminal_color_13 = palette.hex10
-	vim.g.terminal_color_14 = palette.hex10
+	vim.g.terminal_color_9 = palette.hex04
+	vim.g.terminal_color_10 = palette.hex06
+	vim.g.terminal_color_11 = palette.hex08
+	vim.g.terminal_color_12 = palette.hex04
+	vim.g.terminal_color_13 = palette.hex06
+	vim.g.terminal_color_14 = palette.hex08
 	vim.g.terminal_color_15 = palette.hex10
 end
